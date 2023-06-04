e9a1ec71eda2094991680c61aef7ec540faea957
moviebazaa <69888886+moviebazaa@users.noreply.github.com>
2023-06-04 23:26:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,51 +1,36 @@
-import random
-import streamlit as st
-from pytube import YouTube
-import moviepy.editor as mp
+from flask import Flask, render_template, request, redirect, send_file
+import pysrt
+from translate import Translator
 
-# Function for some random animations
-def random_celeb():
-    return random.choice([st.balloons()])
+app = Flask(__name__)
 
-# Function to download YouTube videos in MP3 format
-def download_mp3(url):
-    video_caller = YouTube(url)
-    st.info(video_caller.title, icon="ℹ️")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    # Get the highest quality audio stream
-    audio_stream = video_caller.streams.filter(only_audio=True).order_by('abr').desc().first()
+@app.route('/translate', methods=['POST'])
+def translate():
+    # Get the uploaded .srt file from the form
+    srt_file = request.files['srt_file']
 
-    if audio_stream is not None:
-        # Download the audio stream
-        audio_stream.download()
+    # Read the .srt file using pysrt library
+    subs = pysrt.open(srt_file)
+    
+    # Get the target language from the form
+    target_language = request.form['target_language']
+    
+    # Translate each subtitle in the .srt file
+    translator = Translator(to_lang=target_language)
+    for sub in subs:
+        translated_text = translator.translate(sub.text)
+        sub.text = translated_text
+    
+    # Save the translated .srt file
+    translated_filename = f"translated_{srt_file.filename}"
+    subs.save(translated_filename, encoding='utf-8')
 
-        # Convert the downloaded video to MP3
-        video_filename = audio_stream.default_filename
-        mp3_filename = video_filename.split('.')[0] + '.mp3'
-        video_clip = mp.VideoFileClip(video_filename)
-        audio_clip = video_clip.audio
-        audio_clip.write_audiofile(mp3_filename)
-        audio_clip.close()
-        video_clip.close()
+    # Return the translated file for download
+    return send_file(translated_filename, as_attachment=True)
 
-        # Retrieve video metadata including frame rate
-        video_metadata = mp.VideoFileClip(video_filename)
-        frame_rate = video_metadata.fps
-
-        st.success(f'Done! Frame rate: {frame_rate}')
-        with open(mp3_filename, 'rb') as file:
-            st.download_button(label='Download MP3', data=file, file_name=mp3_filename)
-    else:
-        st.error('Oops! Audio stream is not available!')
-
-# Integration of all above-defined functions
-st.title("YouTube MP3 Downloader")
-url = st.text_input(label="Paste your YouTube URL")
-if st.button("Download"):
-    if url:
-        try:
-            with st.spinner("Loading..."):
-                download_mp3(url)
-            random_celeb()
-        except Exception as e:
-            st.error(e)
+if __name__ == '__main__':
+    app.run(debug=True)
