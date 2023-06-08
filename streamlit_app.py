bab4ef965c242197984e3ccebc598b41d1f65997
moviebazaa <69888886+moviebazaa@users.noreply.github.com>
2023-06-08 18:27:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,26 +1,17 @@
 import telebot
-import pyshorteners
-import time
 
 bot_token = '5772105476:AAGXwZ6JWaEH4NFeRIg6F3oxahAgrXI2cPU'
 bot = telebot.TeleBot(token=bot_token)
 
-def short(url):
-    return pyshorteners.Shortener().tinyurl.short(url)
-
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, 'Hello, world!')
+    bot.reply_to(message, 'Hello! Send me a file, and I will provide you with a direct download link.')
 
-@bot.message_handler(content_types=['photo', 'document'])
+@bot.message_handler(content_types=['document'])
 def file_sent(message):
-    try:
-        bot.send_message(message.chat.id, short(bot.get_file_url(message.document.file_id)))
-    except AttributeError:
-        pass
+    file_id = message.document.file_id
+    file_name = message.document.file_name
+    file_link = bot.get_file_url(file_id)
+    bot.send_message(message.chat.id, f"Download link for {file_name}: {file_link}")
 
-while True:
-    try:
-        bot.polling()
-    except Exception:
-        time.sleep(15)
+bot.polling()
