ee64ff9059afff4c0d7ed1a93e29f2314965bf52
moviebazaa <69888886+moviebazaa@users.noreply.github.com>
2023-06-08 18:05:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import telebot
+import time
 
-bot = '5772105476:AAGXwZ6JWaEH4NFeRIg6F3oxahAgrXI2cPU'
-bot = telebot.TeleBot(token=5772105476:AAGXwZ6JWaEH4NFeRIg6F3oxahAgrXI2cPU)
+bot_token = '5772105476:AAGXwZ6JWaEH4NFeRIg6F3oxahAgrXI2cPU'
+bot = telebot.TeleBot(token=bot_token)
 
-@bot.message_handler(commands['start'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.replay_to(message, 'hello world')
+    bot.reply_to(message, 'Hello, world!')
 
 while True:
     try:
