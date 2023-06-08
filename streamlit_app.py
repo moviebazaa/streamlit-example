cc15179480a5c2a1fb5c40577f27d4b9287e239f
moviebazaa <69888886+moviebazaa@users.noreply.github.com>
2023-06-08 18:23:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,23 @@
 import telebot
+import pyshorteners
 import time
 
 bot_token = '5772105476:AAGXwZ6JWaEH4NFeRIg6F3oxahAgrXI2cPU'
 bot = telebot.TeleBot(token=bot_token)
 
+def short(url):
+    return pyshorteners.Shortener().tinyurl.short(url)
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.reply_to(message, 'Hello, world!')
+
+@bot.message_handler(content_types=['photo', 'document'])
+def file_sent(message):
+    try:
+        bot.send_message(message.chat.id, short(bot.get_file_url(message.document.file_id)))
+    except AttributeError:
+        pass
 
 while True:
     try:
